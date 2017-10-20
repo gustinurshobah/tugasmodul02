9ffaef64c5c19600ef96d7d1f1dcef37ba78036e
gustinurshobah <32300981+gustinurshobah@users.noreply.github.com>
2017-10-20 15:07:08 +0800

    Output bermacam haya

--- praktek0202.cpp
+++ praktek0202.cpp
@@ -11,4 +11,7 @@ int main(){
 	double angka(10.5),Angka,ANGKA(3.14);
 	int a(10),b(7),c(12);
 	
+	cout << nilai<<" : "<<Nilai<<" | "<<Nilai<<" "<<niLAi<<endl;
+	cout << angka <<"\n";
+	cout << Angka <<endl<<ANGKA;
 }

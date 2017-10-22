27ef61cf83a8a796fe028964823bec91a8b849e9
gustinurshobah <32300981+gustinurshobah@users.noreply.github.com>
2017-10-22 22:33:25 +0800

    Meinput luas rumah dan jumlah boc parket

--- praktek0204.cpp
+++ praktek0204.cpp
@@ -3,4 +3,10 @@ using namespace std;
 
 int main(){
 	double lr,p=1.87596,jbp,bp,bpp,a,b,jbph,tb;
+	cout<<"luas rumah : ";
+	cin>>lr;
+	cout<<"jumlah box parket : ";
+	cin>>jbp;
+	cout<<""<<endl;
+	cout<<""<<endl;
 }
